c6fe788724a82398a9a41168265626021191601d
Therese Linden <theresemlinden@gmail.com>
2022-10-11 12:49:18 +0100

    final

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ const resetGame = document.getElementById('resetGame');
 const inputWrapper = document.getElementById('inputWrapper');
 
 let randomNumber = Math.floor(Math.random() * 10) + 1;
-console.log('RANDOM', randomNumber);
-
 let numberOfGuesses = 0;
 
 submitBtn.addEventListener('click', displayResult);
@@ -17,7 +15,6 @@ function playAgain() {
   guesses.innerHTML = '';
 }
 
-// Skapa funktion för att jämföra input värde med rätt svar värde
 function displayResult() {
   let input = document.getElementById('userInput').value;
   const inputField = document.getElementById('userInput');
@@ -28,18 +25,11 @@ function displayResult() {
     resetGame.style.display = 'block';
     inputWrapper.style.display = 'none';
     inputField.value = '';
-    console.log('numberOFGuesses', numberOfGuesses);
-
-    //numberOfGuesses++;
   } else if (input > randomNumber) {
     guesses.innerHTML = 'Gissa på ett lägre nummer';
     inputField.value = '';
-    // numberOfGuesses++;
-    console.log('numberOFGuesses', numberOfGuesses);
   } else {
     guesses.innerHTML = 'Gissa på ett högre nummer';
     inputField.value = '';
-    // numberOfGuesses++;
-    console.log('numberOFGuesses', numberOfGuesses);
   }
 }
